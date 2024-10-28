e5385ef4ff470d6ffe3126d0f1822bf38a4a7e11
Rohit Vishwakarma <63051513+Rohit-0110@users.noreply.github.com>
2024-10-29 00:54:37 +0530

    Update main.tf

--- GitlabWorkspaceProxy/main.tf
+++ GitlabWorkspaceProxy/main.tf
@@ -1,21 +1,3 @@
-provider "aws" {
-  region = var.region
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.workspaces_eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.workspaces_eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.workspaces_eks.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.workspaces_eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.workspaces_eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.workspaces_eks.token
-  }
-}
-
 data "aws_acm_certificate" "issued" {
   domain   = "workspaces.psiquantum.com"
   statuses = ["ISSUED"]
