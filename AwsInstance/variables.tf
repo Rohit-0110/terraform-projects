eedf63ec3dadbaecd17a36bfbc30767bb0678606
rohit <rohit.vishwakarma@msbcgroup.com>
2024-10-28 23:31:18 +0530

    Instance

---
+++ AwsInstance/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "ap-south-1"
+}
+variable "ami" {
+  default = ""
+
+}
+
+variable "instance_type" {
+  default = ""
+
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
